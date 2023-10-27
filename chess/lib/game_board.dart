17967aefd6bed834937b1ff3cfe0f63fe4d91a8a
melisks <meliodassks2001@gmail.com>
2023-10-27 09:55:20 +0530

    new

---
+++ chess/lib/game_board.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class GameBoard extends StatefulWidget {
+  const GameBoard({super.key});
+
+  @override
+  State<GameBoard> createState() => _GameBoardState();
+}
+
+class _GameBoardState extends State<GameBoard> {
+  @override
+  Widget build(BuildContext context) {
+    return GridView.builder(
+      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 8,
+      ),
+      itemCount: 8 * 8,
+      itemBuilder: (BuildContext context, int index) {
+        return;
+      },
+    );
+  }
+}
